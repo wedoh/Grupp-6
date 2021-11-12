31d4743644de84e3e2c842bfd293fafbaaa719f8
Johan Sveide <johan.sveide@gmail.com>
2021-11-12 15:40:32 +0100

    VG List WORKS!!

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var ux = ["User research", "Information architecture", "Interaction design", "Interface design"];
-var arrayLength = ux.length;
+var links = [
+  '<a href="#">News</a>',
+  '<a href="#">About us</a>',
+  '<a href="#">Press</a>',
+  '<a href="#">Careers</a>',
+  '<a href="#">Contact</a>'
+];
+var arrayLength = links.length;
 var myList = "<ul>";
-for (var i = 0; i < arrayLength; i++) {
-myList += "<li>" + ux[i] + "</li>";
+
+for (var i = 0; i < arrayLength; i++)
+{
+  myList += "<li>" + links[i] + "</li>";
 }
 myList += "</ul>";
-document.getElementById("minArrayDiv").innerHTML = myList;
+
+document.getElementById("navbarLinks").innerHTML = myList;
